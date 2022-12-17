7ed6272cc4f917e1c4b09ffaf349985c03d059df
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2022-12-17 21:02:28 +0630

    nav menu scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 console.log("Hello world")
+
+
+// jquery
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         let position = $(this).scrollTop();
+//         if(position > 0){
+//             $("nav").addClass("navmenus");
+//         }else if(position < 200) {
+//             $("nav").removeClass("navmenus");
+//         }
+//     })
+// })
+
+
+//  js
+let getNav = document.querySelector(".navbar");
+console.log(getNav);
+window.addEventListener("scroll",function(){
+    getNav.classList.toggle("navmenus" , window.scrollY > 0);
+})
