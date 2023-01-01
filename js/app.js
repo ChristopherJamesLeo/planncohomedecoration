1057801d51332df92edc5dca0dfd8c00a2853ba8
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2023-01-01 20:26:11 +0630

    18-12-2022

--- js/app.js
+++ js/app.js
@@ -2,21 +2,30 @@ console.log("Hello world")
 
 
 // jquery
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         let position = $(this).scrollTop();
-//         if(position > 0){
-//             $("nav").addClass("navmenus");
-//         }else if(position < 200) {
-//             $("nav").removeClass("navmenus");
-//         }
-//     })
-// })
+$(document).ready(function(){
+
+
+    // Start Nav Bar
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position > 0){
+            $("nav").addClass("navmenus");
+        }else {
+            $("nav").removeClass("navmenus");
+        }
+    })
+
+    $(".navbuttons").click(function(){
+        $(".navbuttons").toggleClass("crossxs");
+    })
+    // End Nav Bar
+
+})
 
 
 //  js
-let getNav = document.querySelector(".navbar");
-console.log(getNav);
-window.addEventListener("scroll",function(){
-    getNav.classList.toggle("navmenus" , window.scrollY > 0);
-})
+// let getNav = document.querySelector(".navbar");
+// console.log(getNav);
+// window.addEventListener("scroll",function(){
+//     getNav.classList.toggle("navmenus" , window.scrollY > 0);
+// })
