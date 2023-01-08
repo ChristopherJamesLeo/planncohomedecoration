81f0043107221805e58bf4c8c92c3fa5bbcf8890
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2023-01-08 21:20:17 +0630

    add light box lib

--- js/app.js
+++ js/app.js
@@ -20,6 +20,29 @@ $(document).ready(function(){
     })
     // End Nav Bar
 
+    // start property section
+    $(".propertylists").click(function(){
+        console.log(this)
+        // $(this).addClass("activeitems"); 
+        $(this).addClass("activeitems").siblings().removeClass("activeitems") // this အား activeitems ထည့်ပြီး siblings() များအား activeitems ဖြုတ်မည်ဟုဆိုထားခြင်းဖြစ်သည် navbar တွင်အသုံးများသည် 
+
+        let ftvalue = $(this).attr("data-filter") // this ၏ data-filter ဟူသော attribute ကို ယူမည်ဖြစ်သည်
+
+        if(ftvalue === "all"){ // ftvalue ၏ value သည် all ဖြစ်ပါက အားလုံးကို ပြပေးမည်ဖြစ်သည်
+            $(".filters").show(); // ပြသည် method
+        } else { // သို့မဟုတ် အားလုံး ကို ဖျောက်ပေးထားမည်ဖြစ်သည် 
+            $(".filters").hide(); // ပြသည် method
+            $(".filters").not("."+ftvalue).hide(); // ftvalue မဟုတ်ခဲ့ပါက hide ပေးမည် 
+            $(".filters").filter("."+ftvalue).show(); // ftvalue အား filter လုပ်ပြီး show လုပ်ပေးမည်ဖြစ်သည် 
+        }
+    })
+    // end property section
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        "showImageNumberLabel" : false ,
+        "wrapAround" : true
+      })
 })
 
 
@@ -29,3 +52,6 @@ $(document).ready(function(){
 // window.addEventListener("scroll",function(){
 //     getNav.classList.toggle("navmenus" , window.scrollY > 0);
 // })
+
+
+// 8BO
