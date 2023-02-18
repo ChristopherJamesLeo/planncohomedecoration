1f016cfc2cb33b626b9010008cf960d0e9df553a
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2023-02-18 21:33:11 +0630

    finish

--- js/app.js
+++ js/app.js
@@ -4,7 +4,19 @@ console.log("Hello world")
 // jquery
 $(document).ready(function(){
 
-
+    // Start Back to top
+    $(".btn-backtotop").hide();
+    $(window).scroll(function(){
+        var getscrolltop = $(this).scrollTop();
+        
+        if(getscrolltop >= 370){
+            $(".btn-backtotop").fadeIn("slow");
+        }else {
+            $(".btn-backtotop").fadeOut(1000);
+        }
+        
+    })
+    // End back to top
     // Start Nav Bar
     $(window).scroll(function(){
         let position = $(this).scrollTop();
@@ -29,20 +41,39 @@ $(document).ready(function(){
         let ftvalue = $(this).attr("data-filter") // this ၏ data-filter ဟူသော attribute ကို ယူမည်ဖြစ်သည်
 
         if(ftvalue === "all"){ // ftvalue ၏ value သည် all ဖြစ်ပါက အားလုံးကို ပြပေးမည်ဖြစ်သည်
-            $(".filters").show(); // ပြသည် method
+            $(".filters").show("slide",500); // ပြသည် method
         } else { // သို့မဟုတ် အားလုံး ကို ဖျောက်ပေးထားမည်ဖြစ်သည် 
             $(".filters").hide(); // ပြသည် method
-            $(".filters").not("."+ftvalue).hide(); // ftvalue မဟုတ်ခဲ့ပါက hide ပေးမည် 
-            $(".filters").filter("."+ftvalue).show(); // ftvalue အား filter လုပ်ပြီး show လုပ်ပေးမည်ဖြစ်သည် 
+            $(".filters").not("."+ftvalue).hide("slide",500); // ftvalue မဟုတ်ခဲ့ပါက hide ပေးမည် 
+            $(".filters").filter("."+ftvalue).show("slide",500); // ftvalue အား filter လုပ်ပြီး show လုပ်ပေးမည်ဖြစ်သည် 
         }
     })
     // end property section
+
+    // Start Adv Section
+    $(window).scroll(function(){
+        var getscrolltt = $(this).scrollTop();
+        if(getscrolltt >= 900){
+            $(".advimages").addClass("fromleft");
+            $(".advtexts").addClass("fromright");
+        }else {
+            $(".advimages").removeClass("fromleft");
+            $(".advtexts").removeClass("fromright");
+        }
+    })
+    // End Adv Section
     lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true,
         "showImageNumberLabel" : false ,
         "wrapAround" : true
       })
+
+    // start footer
+    const getyear = $("#getyear");
+    const getfullyear = new Date().getFullYear();
+    getyear.text(getfullyear);
+    // end footer
 })
 
 
@@ -54,4 +85,4 @@ $(document).ready(function(){
 // })
 
 
-// 8BO
+// 18FN
